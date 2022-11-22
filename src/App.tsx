b1eaa18eb6b447f0aac64421a140b6f118fd768d
Alberto Guaman <80141815+GbrielGarcia@users.noreply.github.com>
2022-11-21 22:09:01 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import useWaifuService  from './hook/useWaifuService'
+import useWaifuService from './hook/useWaifuService'
 
 function App() {
   const waifuHook = useWaifuService();
@@ -7,60 +7,60 @@ function App() {
     <div className="App">
       <button
         onClick={
-         ()=> {
+          () => {
             waifuHook.getRandomWaifu()
             console.log(waifuHook.randomWaifu)
           }
         }
       >
         Get Random Waifu
-        </button>
+      </button>
 
-        {
-          <img src={waifuHook.randomWaifu} alt="" style={{
-            width: '500px',
-         }}/>
-        }
+      {
+        <img src={waifuHook.randomWaifu} alt="" style={{
+          width: '500px',
+        }} />
+      }
 
-        <hr/>
+      <hr />
 
-        <button 
-          onClick={
-            ()=> {
-              waifuHook.getWaifus({type:'nsfw', category: 'neko', unique:false})
-              console.log(waifuHook.waifusList)
-            }
+      <button
+        onClick={
+          () => {
+            waifuHook.getWaifus({ type: 'nsfw', category: 'neko', unique: false })
+            console.log(waifuHook.waifusList)
           }
-        >
-          Get list of waifus
-        </button>
-
-        {
-          waifuHook.waifusList.map((waifu, index) => {
-            return  <img src={waifu} alt="index" key={index} style={{
-              width: '500px',
-           }}/>
-          })
         }
+      >
+        Get list of waifus
+      </button>
 
-        <hr/>
-
-        <button 
-          onClick={
-            ()=> {
-              waifuHook.getWaifus({type:'nsfw', category: 'waifu', unique:true})
-              console.log(waifuHook.waifuUnique)
-            }
-          }
-        >
-          Get unique of waifu
-        </button>
-
-        {         
-         <img src={waifuHook.waifuUnique} alt="index" style={{
+      {
+        waifuHook.waifusList.map((waifu, index) => {
+          return <img src={waifu} alt="index" key={index} style={{
             width: '500px',
-         }}/>
+          }} />
+        })
+      }
+
+      <hr />
+
+      <button
+        onClick={
+          () => {
+            waifuHook.getWaifus({ type: 'nsfw', category: 'waifu', unique: true })
+            console.log(waifuHook.waifuUnique)
+          }
         }
+      >
+        Get unique of waifu
+      </button>
+
+      {
+        <img src={waifuHook.waifuUnique} alt="index" style={{
+          width: '500px',
+        }} />
+      }
     </div>
   );
 }
