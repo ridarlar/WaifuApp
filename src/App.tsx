12f1e694cc9eb05f24f7115fa2056b8d4e5877f7
Alberto Guaman <80141815+GbrielGarcia@users.noreply.github.com>
2022-11-23 18:20:10 -0500

    Grid Wrapper vol 1

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div >
       <Header />
+      <div className='space '></div>
       <Background />
       <Footer />
     </div>
--- src/components/background.tsx
+++ src/components/background.tsx
@@ -5,28 +5,29 @@ import './style/background.css';
 function Background(this: any) {
     const waifuHook = useWaifuService();
 
-
-
     return (
-        <div>
-
+        <section>
             <div className='buttonRefect'>
                 <ReloadOutlined
                     onClick={() => {
-                       waifuHook.getWaifus({
-                        type:'nsfw',
-                        category:'waifu',
-                        unique: false
-                       })
+                        waifuHook.getWaifus({
+                            type: 'sfw',
+                            category: 'waifu',
+                            unique: false
+                        })
                     }}
                     style={{ fontSize: '40px', color: '#08c' }} />
+            </div >
+
+            <div className='gridWrapper' >
+                {
+                    waifuHook.waifusList.map((link, index) => {
+                        return <div className='big'><img alt={link} src={link} key={index} className='image-waifu' /></div>
+                    })
+                }
             </div>
-            {
-                waifuHook.waifusList.map((link, index) => {
-                    return <img alt={link} src={link} key={index} className='image-waifu' style={{ width: '160px'}}/>
-                })
-            }
-        </div>
+
+        </section>
     )
 }
 
